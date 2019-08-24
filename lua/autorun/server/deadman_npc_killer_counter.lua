8371a38e008f95aa5fdd1caf4f157f44e94fbcf5
Deadman <enzo.richard2014@gmail.com>
2019-08-24 08:41:44 +0200

    Update deadman_npc_killer_counter.lua

--- lua/autorun/server/deadman_npc_killer_counter.lua
+++ lua/autorun/server/deadman_npc_killer_counter.lua
@@ -1,22 +1,8 @@
--- Config below
+local shouldResetMoneyOnDeath = DNPC.ShouldResetOnDeath
+local moneyPerNPC = DNPC.MoneyPerNPC
+local blacklistNPC = DNPC.BlacklistNPC
 
-local shoudlresetmoneyondeath = true -- Should we remove the price they won when they spawn (die/change job...)
-local moneyfornpc = 50 -- How much money should we give to the player for each NPC he killed (except the blaclist below)
-local blacklistnpc = { -- NPC that will not give money to players
-	"npc_crow",
-	"npc_seagull",
-	"npc_zombie",
-}
-
-
-
--- DONT TOUCH BELOW THIS LINE      DONT TOUCH BELOW THIS LINE     DONT TOUCH BELOW THIS LINE
--- DONT TOUCH BELOW THIS LINE      DONT TOUCH BELOW THIS LINE     DONT TOUCH BELOW THIS LINE
--- DONT TOUCH BELOW THIS LINE      DONT TOUCH BELOW THIS LINE     DONT TOUCH BELOW THIS LINE
-
-
-
-if shoudlresetmoneyondeath then
+if shouldResetMoneyOnDeath then
 	hook.Add("PlayerSpawn", "DeadmanNPCKillerReset", function(ply)
 		ply:SetNWInt("DeadmanKillerPrice", 0)
 	end)
@@ -24,9 +10,9 @@ end
 
 
 hook.Add("OnNPCKilled", "DeadmanNPCKillerCounter", function(npc, attacker, inflictor)
-	if not attacker:IsPlayer() or not attacker:Alive() or not attacker:IsValid() then return end
-	if not table.HasValue( blacklistnpc, npc:GetClass() ) then
-		attacker:SetNWInt("DeadmanKillerPrice", attacker:GetNWInt("DeadmanKillerPrice", 0) + moneyfornpc)
+	if not attacker:IsValid() or not attacker:IsPlayer() or not attacker:Alive() then return end
+	if not blacklistNPC[npc:GetClass()] then
+		attacker:SetNWInt("DeadmanKillerPrice", attacker:GetNWInt("DeadmanKillerPrice", 0) + moneyPerNPC)
 	end
 
 end)
