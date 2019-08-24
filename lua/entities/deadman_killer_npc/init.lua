695a12f1642980c3902ae516fb0773390ab3516d
Deadman <enzo.richard2014@gmail.com>
2019-08-24 08:35:35 +0200

    Add files via upload

---
+++ lua/entities/deadman_killer_npc/init.lua
@@ -0,0 +1,31 @@
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+function ENT:Initialize()
+
+	self:SetModel("models/props_junk/wood_crate001a.mdl")
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+    self:SetUseType( SIMPLE_USE )
+    local phys = self:GetPhysicsObject()
+    phys:Wake()
+
+end
+
+
+
+function ENT:Use(activator, caller, usetype, value)
+
+	if not activator:IsPlayer() or not activator:Alive() or not activator:IsValid() then return end
+
+	local price = activator:GetNWInt("DeadmanKillerPrice", 0)
+	if price == 0 then
+		activator:ChatPrint("Sorry, you don't have kill any NPC")
+	else
+		activator:addMoney( price )
+		activator:SetNWInt("DeadmanKillerPrice", 0)
+		activator:ChatPrint("You have win $"..price )
+	end
+
+end
---
+++ lua/entities/deadman_killer_npc/shared.lua
@@ -0,0 +1,7 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "NPC"
+ENT.Author = "Deadman"
+ENT.Category = "Deadman NPC Killer"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
